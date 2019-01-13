69ff4cb7139c7fd57e24a41234ad8993b7e22a06
syberside <syberside@gmail.com>
2019-01-13 22:28:30 +0300

    Update cover.sh

--- tool/cover.sh
+++ tool/cover.sh
@@ -3,6 +3,10 @@
 # Fast fail the script on failures.
 set -e
 
+if ! [[ -z "${TRAVIS_TAG}" ]]; then
+    exit 0
+fi
+
 pub global activate dart_coveralls
 
 dart_coveralls report -C -E -T --retry=1 --token=$coverallsToken ./test/activatory_test.dart
